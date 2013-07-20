5dbf15c3dbf1fe7f0711082f19a1fd9fa9f6a2bb
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-07-20 13:04:26 -0500

    Initial Commit

---
+++ TEAM Software CoreData Practice/MainVCAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MainVCAppDelegate.h
+//  TEAM Software CoreData Practice
+//
+//  Created by Ryan Zulkoski on 7/20/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TEAM Software CoreData Practice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TEAM Software CoreData Practice
+//
+//  Created by Ryan Zulkoski on 7/20/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MainVCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MainVCAppDelegate class]));
+    }
+}
